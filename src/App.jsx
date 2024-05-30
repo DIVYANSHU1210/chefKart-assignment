3928070de1115e96209c58b506a1c108a7134133
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2024-05-30 11:36:05 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import MainPage from './Components/MainPage/MainPage'
 import IngredientsPage from './Components/IngridientsPage/IngredientsPage'
 import { Route, Routes } from 'react-router-dom'
 import CartPage from './Components/cartPage/CartPage'
+import NavBar from './Components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>   
+      <NavBar/>
       <Routes>
         <Route path='/' element={<MainPage/>}/>  
         <Route path='/ingredients-page' element={<IngredientsPage/>}/> 
